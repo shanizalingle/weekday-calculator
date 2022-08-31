86361640d2c77b2a7b4a3464d50fd50a40661bda
Shaniza Lingle <shanizalingle@gmail.com>
2022-08-31 11:55:40 -0700

    write Date() test 1
    Co-authored-by: aldeling <aldeling@users.noreply.github.com>
    Co-authored-by: TrevorH08 <TrevorH08@users.noreply.github.com>

---
+++ __tests__/weekday.test.js
@@ -0,0 +1,16 @@
+// export default class Date {
+//   constructor(month, day, year) {
+//     this.month = month;
+//     this.day = day;
+//     this.year = year;
+//   }
+
+//   checkType() {
+
+//   }
+// }
+describe ("Date", () => {
+  test("it should return nothing", () => {
+    const date = new Date();
+  });
+});
