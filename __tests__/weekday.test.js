cd1077e29aa4df1ca3635a8b2094c893b03465b1
Shaniza Lingle <shanizalingle@gmail.com>
2022-09-01 11:14:03 -0700

    write calculateDay() test 2
    Co-authored-by: TrevorH08 <TrevorH08@users.noreply.github.com>

--- __tests__/weekday.test.js
+++ __tests__/weekday.test.js
@@ -1,9 +1,15 @@
 import { calculateDay } from '../src/js/weekday.js';
 
 describe ('calculateDay', () => {
+
   test("determine which day of the week the inputted day is", () => {
     const date = "08/31/2022";
     expect(calculateDay(date)).toEqual(3);
+  });
+
+  test("return a string based on the day of the week", () => {
+    const date = "08/31/2022";
+    expect(calculateDay(date)).toEqual("This date lands on a Wednesday");
   });
 });
 
