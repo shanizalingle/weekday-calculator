ca280a78eb04bf602fcb79937948c2bae8e60d44
Shaniza Lingle <shanizalingle@gmail.com>
2022-09-01 09:49:06 -0700

    write calculateDay()test 1
    Co-authored-by: TrevorH08 <TrevorH08@users.noreply.github.com>

--- __tests__/weekday.test.js
+++ __tests__/weekday.test.js
@@ -1,16 +1,9 @@
-// export default class Date {
-//   constructor(month, day, year) {
-//     this.month = month;
-//     this.day = day;
-//     this.year = year;
-//   }
+import { calculateDay } from '../src/js/weekday.js';
 
-//   checkType() {
-
-//   }
-// }
-describe ("Date", () => {
-  test("it should return nothing", () => {
-    const date = new Date();
+describe ('calculateDay', () => {
+  test("determine which day of the week the inputted day is", () => {
+    const date = "08/31/2022";
+    expect(calculateDay(date)).toEqual(4);
   });
 });
+
