4c872885c37648da27d0ea8956a0be6c2801b6ad
Shaniza Lingle <shanizalingle@gmail.com>
2022-09-01 16:36:16 -0700

    update HTML, refactor dateString() and calculateDay()
    Co-authored-by: TrevorH08 <TrevorH08@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import { calculateDay, dateString } from '../src/js/weekday.js';
+
+function handleSubmission(event){
+  event.preventDefault();
+  const inputDate = document.querySelector("input#dateInput").value;
+  document.querySelector("span#resultOutputSpan").innerText = dateString(calculateDay(inputDate));
+}
+
+window.addEventListener("submit", handleSubmission); 
--- src/js/weekday.js
+++ src/js/weekday.js
@@ -1,28 +1,25 @@
-export const calculateDay = () => {
-  let inputDate = "August 31, 2022"
+export const calculateDay = (inputDate) => {
   let userDate = new Date(inputDate).getDay();
   return userDate;
 };
 
-export const dateString = () => {
-  let day = calculateDay();
-  if ( day === 0) {
+export const dateString = (inputDate) => {
+  if ( inputDate === 6) {
     return "This date lands on a Sunday";
-  } else if ( day === 1) {
-    return "This date lands on a Monday";
-  } else if ( day === 2) {
-    return "This date lands on a Tuesday";
-  } else if ( day === 3) {
-    return "This date lands on a Wednesday";
-  } else if ( day === 4) {
-    return "This date lands on a Thursday";
-  } else if ( day === 5) {
+  } else if ( inputDate === 5) {
+    return "This date lands on a Saturday";
+  } else if ( inputDate === 4) {
     return "This date lands on a Friday";
-  } else if ( day === 6) {
-    return "This date lands on a Saturday"
+  } else if ( inputDate === 3) {
+    return "This date lands on a Thursday";
+  } else if ( inputDate === 2) {
+    return "This date lands on a Wednesday";
+  } else if ( inputDate === 1) {
+    return "This date lands on a Tuesday";
+  } else if ( inputDate === 0) {
+    return "This date lands on a Monday";
   } else {
-    return "Make sure the date you entered is correct"
+    return "Make sure the date you entered is correct";
   }
-}
-
+};
 
